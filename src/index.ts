e61e60a12529737fefc967e8ae965cc23ad5a2ea
arcetros <75669794+arcetros@users.noreply.github.com>
2022-11-28 17:32:39 +0800

    fix(workflows): drop node verion 10.x

--- src/index.ts
+++ src/index.ts
@@ -3,48 +3,45 @@ import { Options } from './types';
 import { isValidHttpUrl } from './utils';
 
 const defaultOptions = {
-  maxRedirects: 5,
-  lang: '*',
-  timeout: 10000,
-  forceImageHttps: true,
-  customRules: {},
+    maxRedirects: 5,
+    lang: '*',
+    timeout: 10000,
+    forceImageHttps: true,
+    customRules: {},
 };
 
-const getRecipeData = async (
-  input: string | Partial<Options>,
-  inputOptions: Partial<Options> = {}
-) => {
-  let siteUrl;
+const getRecipeData = async (input: string | Partial<Options>, inputOptions: Partial<Options> = {}) => {
+    let siteUrl;
 
-  if (typeof input === 'object') {
-    inputOptions = input;
-    siteUrl = input.url;
-  } else {
-    siteUrl = input;
-  }
+    if (typeof input === 'object') {
+        inputOptions = input;
+        siteUrl = input.url;
+    } else {
+        siteUrl = input;
+    }
 
-  if (!isValidHttpUrl(siteUrl as string)) {
-    throw new Error('url must start with http:// or https://');
-  }
+    if (!isValidHttpUrl(siteUrl as string)) {
+        throw new Error('url must start with http:// or https://');
+    }
 
-  const options = Object.assign({}, defaultOptions, inputOptions);
+    const options = Object.assign({}, defaultOptions, inputOptions);
 
-  let html;
+    let html;
 
-  if (!options.url) {
-    const response = await axios(siteUrl as string, {
-      headers: {
-        'Accept-Language': options.lang,
-      },
-      timeout: options.timeout,
-      maxRedirects: options.maxRedirects,
-    });
-    html = response.data;
-  } else {
-    html = options.html;
-  }
+    if (!options.url) {
+        const response = await axios(siteUrl as string, {
+            headers: {
+                'Accept-Language': options.lang,
+            },
+            timeout: options.timeout,
+            maxRedirects: options.maxRedirects,
+        });
+        html = response.data;
+    } else {
+        html = options.html;
+    }
 
-  console.log(html);
+    console.log(html);
 };
 
 export default getRecipeData;
