b613c9489bfe04b8ddf4c895d6d4f1dd29e41ba0
arcetros <75669794+arcetros@users.noreply.github.com>
2022-11-30 16:01:58 +0800

    update package.json keywords and urls

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,8 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import getRecipeData from '../dist';
+// import getRecipeData from '../dist';
+import getRecipeData from 'scrape-recipe-schema';
 
 const App = () => {
     React.useEffect(() => {
